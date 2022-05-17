c202ea39186973f85c50a73d8ecdbb30f5ca4733
Lena Schneider <lena.schneider407@gmail.com>
2022-05-18 00:13:28 +0200

    task manager CRUD complete

--- db/migrate/20220517124412_add_importance_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddImportanceToTasks < ActiveRecord::Migration[7.0]
-  def change
-  	add_column :tasks, :importance, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_17_124412) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_17_182340) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "importance"
+    t.boolean "completed", default: false
   end
 
 end
