bec2028d9f11c4fc7512a24679982e605784ef41
Lena Schneider <lena.schneider407@gmail.com>
2022-05-18 13:37:17 +0200

    task manager CRUD complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#list'
 
+  # 2 for Create
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
 
   get 'tasks/:id', to: 'tasks#show', as: :task
 
+  # 2 for Update
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
 
